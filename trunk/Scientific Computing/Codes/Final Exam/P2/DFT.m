bf1f8d22f2116a9812ad477bc4e4a2ef55f4d087
trungvdt49 <trungvdt49@39687aed-0682-cb52-9076-1d4f63bc726f>
2014-01-11 09:00:08 +0000

    Final Exam

---
+++ trunk/Scientific Computing/Codes/Final Exam/P2/DFT.m
@@ -0,0 +1,13 @@
+  function y = DFT(x)
+% y = DFT(x)
+% y is the discrete Fourier transform of a column n-vector x.  
+
+n = length(x);
+y = x(1)*ones(n,1);
+if n > 1
+   v = exp(-2*pi*sqrt(-1)/n).^(0:n-1)';
+   for k=2:n
+      z = rem((k-1)*(0:n-1)',n ) +1;
+      y = y + v(z)*x(k);
+   end
+end
